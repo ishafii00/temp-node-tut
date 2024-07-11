3771ff3316a089c56716d7386f57fd4b2ac1bace
Indira Shafii <ishafii@Indiras-Laptop.local>
2024-07-11 17:09:15 -0400

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+// GLOBALS - NO WINDOW
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules
+// module - info about current module
+// process - info about env where program is being executed
+console.log(__dirnane)
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'SUPER SECRET'
+// sharing john and peter through module.exports
+const john = 'john'
+const peter = 'peter'
+
+module.exports =  {john,peter}
